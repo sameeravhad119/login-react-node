75d0ec63dcb39546825637ce198f28294011945b
Sameer Avhad <sameeravhad119@gmail.com>
2022-01-02 14:25:11 +0530

    mearn prototype. Build CRA. Run server. Server is serving index html(React).That means we stopped using react development server running on 3000.And only started node server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
 
 const NODE_ENV = process.env.NODE_ENV;
 if (NODE_ENV === "development") {
@@ -12,13 +13,20 @@ if (NODE_ENV === "development") {
   dotenv.config({ path: pathname });
 }
 
-const PORT = process.env.PORT;
+const PORT = process.env.BACKEND_PORT;
 app.listen(PORT, () => {
   console.log(`app is listening on ${PORT}`);
 });
 
-app.get("/*", (req, res, next) => {
+//serving build folder statically. CRA DEployment plus codewithHarry Backend Tutorial: Serving HTML Files using NodeJs | Web Development Tutorials #66 reference
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+let indexHtml = path.resolve(__dirname, "..", "build/index.html");
+console.log(`indexHtml`, indexHtml);
+
+app.get("/", (req, res, next) => {
   res.setHeader("Content-Type", "text/html");
   res.status(200);
-  res.send("<h1>App is Running</h1>");
+  res.send(indexHtml);
+  // res.send("<h1>App is Running</h1>");
 });
